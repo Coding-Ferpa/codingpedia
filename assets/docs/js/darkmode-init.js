2ee58cb567a831fcd3d6a91fcd800679841bef7f
gbiz0 <jardimbgustavo@gmail.com>
2026-06-26 14:07:03 -0300

    feat: update template

--- assets/docs/js/darkmode-init.js
+++ assets/docs/js/darkmode-init.js
@@ -1,9 +1,12 @@
 const globalDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const localMode = localStorage.getItem('theme');
 
-if (localMode === null) {
-  localStorage.setItem('theme', 'dark');
+if (localMode === 'dark') {
   document.documentElement.setAttribute('data-dark-mode', '');
-} else if (localMode === 'dark') {
+} else if (localMode === 'light') {
+  document.documentElement.removeAttribute('data-dark-mode');
+} else if (globalDark) {
   document.documentElement.setAttribute('data-dark-mode', '');
+} else {
+  document.documentElement.removeAttribute('data-dark-mode');
 }
